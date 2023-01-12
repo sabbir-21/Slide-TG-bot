d18c32d636e7018738cb56b6845a33f5e6cbd690
Sabbir Ahmed <mdsabbirahmed25@gmail.com>
2023-01-12 16:36:39 +0600

    fixed bugs
    fixed encoding="utf-8"
    fixed not removing file after error
    some strings change

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ async def anydl(bot, message):
 
 @webdl.on_message((filters.regex("https") | filters.regex("http") | filters.regex("www")) & (filters.regex('slideshare')))
 async def slideshare(bot, message):
-    txt = await message.reply_text("Validating Link", quote=True)
+    txt = await message.reply_text("Slideshare Link Detected", quote=True)
     try:
         url = re.findall(r'\bhttps?://.*[(slideshare)]\S+', message.text)[0]
         url = url.split("?")[0]
@@ -90,26 +90,37 @@ async def slideshare(bot, message):
 
         #await message.reply_document(filename+".pdf", caption=f"**File:** {filename}.pdf\nUpload requested by {message.chat.first_name}\n"f"Developed by: **Sabbir Ahmed** @sabbir21", quote=True)
         await message.reply_document(filename+".pdf", caption=f"**File:** {filename}.pdf\n"f"Developed by: **Sabbir Ahmed** @sabbir21", quote=True)
+        await txt.delete()
         #text parse
         
-        file_write = open(filename+'.txt', 'a+')
+        file_write = open(filename+'.txt', 'a+', encoding="utf-8")
         for yy in soup.find_all('ol'):
             file_write.write(f"{yy.text}\n\n")
         file_write.close()
         await message.reply_document(f'{filename}.txt', caption=f"**Text Contents:** {filename}.txt\n"f"Developed by: **Sabbir Ahmed** @sabbir21", quote=True)
-        await txt.delete()
+        
+        try:
+            shutil.rmtree(folder_name)
+            os.remove(filename+".pdf")
+            os.remove(filename+".txt")
+            
+        except: pass
     except Exception as error:
         print(error)
         await message.reply_text(text=f"{error}", disable_web_page_preview=True, quote=True)
-        await txt.delete()
+        #await txt.delete()
+        try:
+            shutil.rmtree(folder_name)
+            os.remove(filename+".pdf")
+            os.remove(filename+".txt")
+            
+        except: pass
         return
-    os.remove(filename+".pdf")
-    os.remove(filename+".txt")
-    shutil.rmtree(folder_name)
+    
 
 @webdl.on_message((filters.regex("https") | filters.regex("http") | filters.regex("www")) & (filters.regex('slideplayer')))
 async def slideplayer(bot, message):
-    txt2 = await message.reply_text("Validating Slideplayer Link", quote=True)
+    txt2 = await message.reply_text("Slideplayer Link Detected", quote=True)
     try:
         url = re.findall(r'\bhttps?://.*[(slideplayer)]\S+', message.text)[0]
         url = url.split("?")[0]
@@ -140,19 +151,81 @@ async def slideplayer(bot, message):
         pdfimages[0].save(pdf_path, "PDF" , resolution=100.0, save_all=True, append_images=pdfimages[1:])
 
         await message.reply_document(filename+".pdf", caption=f"**File:** {filename}.pdf\n"f"Developed by: **Sabbir Ahmed** @sabbir21", quote=True)
-        
         await txt2.delete()
+        try:
+            shutil.rmtree(folder_name)
+            os.remove(filename+".pdf")
+        except: pass
     except Exception as error:
         print(error)
         await message.reply_text(text=f"{error}", disable_web_page_preview=True, quote=True)
         await txt2.delete()
+        try:
+            shutil.rmtree(folder_name)
+            os.remove(filename+".pdf")
+        except: pass
         return
-    os.remove(filename+".pdf")
-    shutil.rmtree(folder_name)
+    
+
+#slideserve
+@webdl.on_message((filters.regex("https") | filters.regex("http") | filters.regex("www")) & (filters.regex('slideserve')))
+async def slideserve(bot, message):
+    txt2 = await message.reply_text("Slideserve Link Detected", quote=True)
+    try:
+        
+        url = message.text
+        #url = url.split("?")[0]
+        folder_name = str(round(time.time()))
+        filename = 'alu'
+        r = requests.get(url)
+        await txt2.edit(text=f"Downloading from slideserve", disable_web_page_preview=True)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        #images = soup.findAll('img', attrs = {'srcset' : True})
+        os.mkdir(folder_name)
+        #images = soup.find_all('img')
+        #loop fn
+        i=0
+        for img in soup.find_all('a'):
+            images = img.get('href').split("//")[1]
+            images = "http://" + images
+            i += 1
+            r = requests.get(images).content
+            open(f"{folder_name}/images_{i}.jpg", "wb+").write(r)
+        await txt2.edit(text=f"Uploading to telegram", disable_web_page_preview=True)
+        #pdf making
+        
+        file_names = os.listdir(folder_name) #make folder
+        file_names = natsorted(file_names)
+        pdfimages = [Image.open(f"{folder_name}/{f}") for f in file_names]
+        pdf_path = filename + '.pdf'
+        pdfimages[0].save(pdf_path, "PDF" , resolution=100.0, save_all=True, append_images=pdfimages[1:])
+        
+        await message.reply_document(filename+".pdf", caption=f"**File:** {filename}.pdf\n"f"Developed by: **Sabbir Ahmed** @sabbir21", quote=True)
+        
+        file_write = open(filename+'.txt', 'a+', encoding="utf-8")
+        for yy in soup.find_all('ol'):
+            file_write.write(yy.text+'\n\n')
+        file_write.close()
+        await message.reply_document(f'{filename}.txt', caption=f"**Text Contents:** {filename}.txt\n"f"Developed by: **Sabbir Ahmed** @sabbir21", quote=True)
+        await txt2.delete()
+        try:
+            shutil.rmtree(folder_name)
+            os.remove(filename+".pdf")
+        except: pass
+    except Exception as error:
+        print(error)
+        await message.reply_text(text=f"{error}", disable_web_page_preview=True, quote=True)
+        await txt2.delete()
+        try:
+            shutil.rmtree(folder_name)
+            os.remove(filename+".pdf")
+        except: pass
+        return
+    
 #any dl fn
 @webdl.on_message((filters.regex("https") | filters.regex("http") | filters.regex("www")))
 async def anydl(bot, message):
-    txt2 = await message.reply_text("Validating the Link", quote=True)
+    txt2 = await message.reply_text("Direct Link Detected", quote=True)
     try:
 
         url = re.findall(r'\bhttps?://.*\S+', message.text)[0]
@@ -171,8 +244,10 @@ async def anydl(bot, message):
         print(error)
         await message.reply_text(text=f"{error}", disable_web_page_preview=True, quote=True)
         await txt2.delete()
+        os.remove(output.name)
         return
     os.remove(output.name) 
+     
 
 
 #main fn run
